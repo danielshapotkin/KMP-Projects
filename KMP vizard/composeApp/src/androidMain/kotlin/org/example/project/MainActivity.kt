f806b40805a352761e65c2a8be7dbe57ae0f83b3
DanielShapotkin <danielshapotkin@gmail.com>
2024-07-17 18:43:36 +0300

    update

--- KMP vizard/composeApp/src/androidMain/kotlin/org/example/project/MainActivity.kt
+++ KMP vizard/composeApp/src/androidMain/kotlin/org/example/project/MainActivity.kt
@@ -1,18 +1,31 @@
 package org.example.project
 
 import App
+import MainViewModel
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Divider
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.viewmodel.compose.viewModel
+import io.ktor.websocket.Frame
+import java.lang.reflect.Modifier
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
-            App()
+            Appp()
+
         }
     }
 }
@@ -22,3 +35,17 @@ class MainActivity : ComponentActivity() {
 fun AppAndroidPreview() {
     App()
 }
+@Composable
+fun Appp(mainViewModel: MainViewModel = viewModel()) {
+    MaterialTheme {
+        val greetings by mainViewModel.greetingList.collectAsStateWithLifecycle()
+
+        Column(
+            verticalArrangement = Arrangement.spacedBy(20.dp),
+        ) {
+            greetings.forEach { greeting ->
+                Text(greeting)
+                Divider()
+            }
+        }
+    }}
