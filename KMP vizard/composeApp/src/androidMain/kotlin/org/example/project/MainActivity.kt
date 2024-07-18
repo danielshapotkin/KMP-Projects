781b8a1ce39a2cbed3bb3329040b28675e9d9e9a
DanielShapotkin <danielshapotkin@gmail.com>
2024-07-18 18:39:17 +0300

    #3 start

--- KMP vizard/composeApp/src/androidMain/kotlin/org/example/project/MainActivity.kt
+++ KMP vizard/composeApp/src/androidMain/kotlin/org/example/project/MainActivity.kt
@@ -1,31 +1,34 @@
 package org.example.project
 
-import App
 import MainViewModel
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
-import io.ktor.websocket.Frame
-import java.lang.reflect.Modifier
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
-            Appp()
-
+            ListItem("", "")
         }
     }
 }
@@ -33,19 +36,20 @@ class MainActivity : ComponentActivity() {
 @Preview
 @Composable
 fun AppAndroidPreview() {
-    App()
+
 }
 @Composable
-fun Appp(mainViewModel: MainViewModel = viewModel()) {
-    MaterialTheme {
-        val greetings by mainViewModel.greetingList.collectAsStateWithLifecycle()
+fun ListItem(name: String, prof: String) {
+   Card(
+       modifier = Modifier.fillMaxWidth().padding(10.dp),
+       shape = RoundedCornerShape(15.dp),
+       elevation = 5.dp
+   ){
+        Box(
+            modifier = Modifier.fillMaxSize()
 
-        Column(
-            verticalArrangement = Arrangement.spacedBy(20.dp),
-        ) {
-            greetings.forEach { greeting ->
-                Text(greeting)
-                Divider()
-            }
+        ){
+            Text(text= "sdyhgdrt")
         }
-    }}
+   }
+}
