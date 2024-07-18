5d9e81a72130d3922226de462b5ae69df6448de8
DanielShapotkin <danielshapotkin@gmail.com>
2024-07-18 14:02:48 +0300

    Operator When

--- app/src/main/java/com/example/kotlin/MainActivity.kt
+++ app/src/main/java/com/example/kotlin/MainActivity.kt
@@ -15,37 +15,23 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         findViewById<TextView>(R.id.tw).text = "fghj"
-        test(null)
-        val array = arrayOf(1, 2, 3, 4, 5) // Массив из 5 элементов, каждый элемент равен 0
-
-            Log.d("4", "3dkm ${2+2}")
-
-        val items = listOf("apple", "banana", "orange")
-        mutableListOf("apple", "banana", "orange")
-
-        for (item in items){
-            Log.d("", item)
-        }
-        var index = 0
-        while (index<items.size){
-            Log.d("", items[index])
-            index++
-        }
-    fun test (str: String){
-        Log.d("", str)
-    }
+        testWhen(1)
 
 
     }
-    fun test(vararg words: String?) {
-        words.forEach {
-            val result = it ?: "String is null"//Elvis опертаор
-            Log.d("", result)
 
-
-            //When
-
+    fun testWhen(input: Any) {
+        when(input) {
+        1-> Log.d("", "1")
+        is String -> Log.d("", "Строка")
+        else -> Log.d("", "Something else")
         }
+    }
+
+
+
+
+
     }
 
 }
